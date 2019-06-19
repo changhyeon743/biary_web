96542898f164cbb9d3a6b66fbb23750d2c18b88a
changhyeon743 <changhyeon743@gmail.com>
2019-06-19 21:29:42 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -8,10 +8,10 @@ import BookView from './BookView'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<Router history = {browserHistory}>
+<BrowserRouter>
     <App/>
     <BookView/>
- </Router>, document.getElementById('root'));
+ </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
