76f9de95fb747bb569072eb990d8d863c5e5e78d
changhyeon743 <changhyeon743@gmail.com>
2019-06-19 23:18:59 +0900

    commit

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -6,15 +6,15 @@ export default class Popup extends React.Component {
     render() {
         return (
             <div className='popup'>
-                <div className='popup\_inner'>
+                <div className='popup_inner'>
 
-                    {this.props.bookDatas != null && this.props.bookDatas[0].data.books.map(book => {
+                    {/* {this.props.bookDatas != null && this.props.bookDatas[0].data.books.map(book => {
                         return <Book
                             key={book.token}
                             imgLink={book.imageURL}
                             title={book.title}
                         />
-                    })}
+                    })} */}
                     <button onClick={this.props.closePopup}>close me</button>
 
                 </div>
